51a695c90a3846d9a78b7acf2bede5bcb0cb616a
GabrielaLopVar <angeleslopezvargs17@gmail.com>
2026-02-11 09:29:11 -0400

    Add palabras.js with vocabulary categories

---
+++ public/palabras.js
@@ -0,0 +1,32 @@
+```python
+const palabras = {
+
+  comida: [
+    "Pizza",
+    "Hamburguesa",
+    "Pasta",
+    "Arepa",
+    "Sushi",
+    "Ensalada"
+  ],
+
+  lugares: [
+    "Playa",
+    "Hospital",
+    "Escuela",
+    "Aeropuerto",
+    "Cine",
+    "Restaurante"
+  ],
+
+  animales: [
+    "Perro",
+    "Gato",
+    "Delfín",
+    "Águila",
+    "Elefante",
+    "León"
+  ]
+  ```
+
+};
